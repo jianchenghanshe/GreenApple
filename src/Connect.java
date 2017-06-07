b3cbed2de439626906d835e047e271386922b3b7
zhq <89786632@qq.com>
2017-06-07 23:58:05 +0800

    Hello

---
+++ src/Connect.java
@@ -0,0 +1,88 @@
+import java.io.File;
+import java.net.URL;
+
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import junit.framework.TestCase;
+
+public class Connect {
+	//屏幕的宽高
+	private int width;
+	private int height;
+    private AppiumDriver driver;
+  
+
+
+    public Connect()throws Exception {  
+    	
+
+        File classpathRoot = new File(System.getProperty("user.dir"));  
+        File appDir = new File(classpathRoot, "apps");  
+        System.out.println(appDir);
+        File app = new File(appDir, "xinwenzhuan-production-1.1.3.2.apk");  
+        
+        DesiredCapabilities capabilities = new DesiredCapabilities();  
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "");  
+
+        capabilities.setCapability("platformName", "Android");  
+        capabilities.setCapability("deviceName","621QECPR2LBAD");  
+
+        capabilities.setCapability("platformVersion", "4.4");  
+        capabilities.setCapability("app", app.getAbsolutePath());  
+        capabilities.setCapability("newCommandTimeout", "300");  //设置收到下一条命令的超时时间,超时appium会自动关闭session ,默认60秒
+        capabilities.setCapability("noReset", true); //不需要再次安装
+        capabilities.setCapability("appPackage", "com.coohua.xinwenzhuan"); 
+
+        capabilities.setCapability("appActivity", ".controller.MainActivity");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);  
+
+        //获得屏幕的宽高
+        height = driver.manage().window().getSize().height;
+        width = driver.manage().window().getSize().width;
+
+
+    }
+    
+    //提供driver返回函数
+    public AppiumDriver getDriver(){
+    	return driver;
+    }
+    //提供屏幕的宽和高
+    public int getWidth(){
+    	return width;
+    }
+    public int getHeight(){
+    	return height;
+    }
+    
+    public static void main(String[] args){
+    	try {
+			Connect con = new Connect();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/MyMethod.java
@@ -0,0 +1,52 @@
+import io.appium.java_client.AppiumDriver;
+
+public class MyMethod {
+	
+	private Connect con;
+	public MyMethod(Connect con){
+		this.con = con;
+	}
+	//打印信息
+	public void echo(String msg){
+		System.out.println(msg);
+	}
+	//等待函数
+	public void slp(int times){
+		echo("wait for "+times+"s……");
+		try {
+			Thread.sleep(times*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		echo("end the waitting time");
+	}
+	//以中心点滑动
+    public void my_swipe(int direction,double range){
+    	if(range > 0.48) range = 0.48;
+    	if(range < 0) range = 0;
+    	int width = con.getWidth();
+    	int height = con.getHeight();
+    	AppiumDriver driver = con.getDriver();
+    	switch(direction){
+    	case 0:
+    		echo("向上滑动了："+(int)(height*range)+"像素");
+    		driver.swipe(width/2, height/2, width/2, (int)(height/2-height*range), 200);
+    		break;
+    	case 1:
+    		echo("向右滑动了："+(int)(width*range)+"像素");
+    		driver.swipe(width/2, height/2,(int)(width/2+width*range),height/2 , 200);
+    		break;
+    	case 2:
+    		echo("向下滑动了："+(int)(height*range)+"像素");
+    		driver.swipe(width/2, height/2,width/2,(int)(height/2+height*range), 200);
+    		break;
+    	case 3:
+    		echo("向左滑动了："+(int)(width*range)+"像素");
+    		driver.swipe(width/2,height/2,(int)(width/2-width*range),height/2, 200);
+    		break;
+    	}
+    }
+    
+
+}
